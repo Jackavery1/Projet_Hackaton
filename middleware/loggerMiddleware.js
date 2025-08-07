d7d3330b500637835d0746561835d8b19cb707c9
MartinezJoris <jorisdavid.martinez@gmail.com>
2025-08-07 17:31:16 +0200

    Commit final : Routes login/register/liste/logout ok et sécurisées

--- middleware/loggerMiddleware.js
+++
@@ -1,5 +0,0 @@
-module.exports = (req, res, next) => {
-    const user = req.user ? `@${req.useer.username}` : "anonymous";
-    console.log(`[${req.method}] ${req.url} - ${user}`);
-    next();
-};
